af2be106c9043a67ed1b49cf14f3711224d9e6d2
Hardik Ojha <44747868+kidrahahjo@users.noreply.github.com>
2020-12-30 01:20:29 +0530

    Complete unresponsive website

---
+++ resources/scripts/helper.js
@@ -0,0 +1,18 @@
+function open_link(name) {
+    if (name == "email") {
+        window.open("mailto:kidrahahjo@gmail.com", "_blank");
+    }
+    else if (name == "linkedin") {
+        window.open("https://linkedin.com/in/kidrahahjo", "_blank");
+    }
+    else if (name == "twitter") {
+        window.open("https://twitter.com/kidrahahjo", "_blank");
+    }
+    else if (name == "facebook") {
+        window.open("https://facebook.com/kidrahahjo", "_blank");
+    }
+    else{
+        console.log('Invalid parameter + ' + name + 'fail');
+    }
+    return false;
+  }
