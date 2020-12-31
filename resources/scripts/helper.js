8a0c29693531bb8cf9118cc4618b69144175ebba
Hardik Ojha <44747868+kidrahahjo@users.noreply.github.com>
2020-12-31 11:38:05 +0530

    Add links on cards

--- resources/scripts/helper.js
+++ resources/scripts/helper.js
@@ -1,4 +1,5 @@
 function open_link(name) {
+    console.log(name)
     if (name == 'resume') {
         window.open("resources/resume/hardik-resume.pdf", "_blank");
     }
@@ -14,8 +15,29 @@ function open_link(name) {
     else if (name == "facebook") {
         window.open("https://facebook.com/kidrahahjo", "_blank");
     }
-    else{
-        console.log('Invalid parameter + ' + name + 'fail');
+    else if (name == "thesplash") {
+        window.open("http://jointhesplash.com/", "_blank");
+    }
+    else if (name == "signac") {
+        window.open("http://signac.io/", "_blank");
+    }
+    else if (name == "gsoc") {
+        window.open("http://summerofcode.withgoogle.com/", "_blank");
+    }
+    else if (name == "ashwinisharma") {
+        window.open("https://www.iitr.ac.in/~CH/Ashwini_Kumar_Sharma", "_blank");
+    }
+    else if (name == "thomso") {
+        window.open("https://thomso.in", "_blank");
+    }
+    else if (name == "smp") {
+        window.open("https://smp.iitr.ac.in/", "_blank");
+    }
+    else if (name == "arp") {
+        window.open("http://arp.iitr.ac.in/", "_blank");
+    }
+    else if (name == "saciitr") {
+        window.open("https://sac.iitr.ac.in/", "_blank");
     }
     return false;
   }
